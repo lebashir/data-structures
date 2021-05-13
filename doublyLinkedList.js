b59559fd91ade77e3f447d7f95aa34f7ed85a7ac
lebashir <bashir.abihabib@gmail.com>
2021-05-13 08:39:26 -0500

    added get method

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -67,7 +67,29 @@ class DoublyLinkedList {
         }
         this.length++;
         return this;
+    }
+    get(index) {
+        if (index < 0 || index >= this.length) return null;
         
+        if (index <= this.length/2) {
+            let count = 0;
+            let current = this.head;
+            while (count != index) {
+                current = current.next;
+                count++
+            }
+            return current;
+        } else {
+            
+            let count = this.length - 1;
+            let current = this.tail;
+            while(count != index) {
+                current = current.previous;
+                count--;
+            }
+            return current;
+        }
     }
 }
 
+ 
