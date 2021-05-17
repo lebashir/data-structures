2b8df7acd9d246442e9143457b029b046474cd64
lebashir <bashir.abihabib@gmail.com>
2021-05-17 14:12:25 -0500

    added keys and values methods to hash-table class

--- hashTable.js
+++ hashTable.js
@@ -4,38 +4,61 @@ class HashTable {
     }
   
     _hash(key) {
-      let total = 0;
-      let WEIRD_PRIME = 31;
-      for (let i = 0; i < Math.min(key.length, 100); i++) {
-        let char = key[i];
-        let value = char.charCodeAt(0) - 96
-        total = (total * WEIRD_PRIME + value) % this.keyMap.length;
-      }
-      return total;
+        let total = 0;
+        let WEIRD_PRIME = 31;
+        for (let i = 0; i < Math.min(key.length, 100); i++) {
+            let char = key[i];
+            let value = char.charCodeAt(0) - 96
+            total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+        }
+        return total;
     }
-      set(key, value) {
-          let index = this._hash(key);
-          if (!this.keyMap[index]) {
-             this.keyMap[index] = []
-          }
-          this.keyMap[index].push([key, value]);
+    set(key, value) {
+        let index = this._hash(key);
+        if (!this.keyMap[index]) {
+            this.keyMap[index] = []
+        }
+        this.keyMap[index].push([key, value]);
           
    
-           return this.keyMap;
-      }
-      get(key) {
-          let index = this._hash(key);
-          if(this.keyMap[index]) {
-              for (let pair of this.keyMap[index]) {
-                  if(pair[0] === key) {
-                      return pair[1]
-                  }
+        return this.keyMap;
+    }
+    get(key) {
+        let index = this._hash(key);
+        if(this.keyMap[index]) {
+            for (let pair of this.keyMap[index]) {
+                if(pair[0] === key) {
+                    return pair[1]
+                }
+            }
+        }
+        return undefined;
+    }
+    keys(){
+        let keysArr = [];
+        for(let i = 0; i < this.keyMap.length; i++){
+          if(this.keyMap[i]){
+            for(let j = 0; j < this.keyMap[i].length; j++){
+              if(!keysArr.includes(this.keyMap[i][j][0])){
+                keysArr.push(this.keyMap[i][j][0])
               }
+            }
           }
-          return undefined;
-          
-  
+        }
+        return keysArr;
       }
-     
-  }
+      values(){
+        let valuesArr = [];
+        for(let i = 0; i < this.keyMap.length; i++){
+          if(this.keyMap[i]){
+            for(let j = 0; j < this.keyMap[i].length; j++){
+              if(!valuesArr.includes(this.keyMap[i][j][1])){
+                valuesArr.push(this.keyMap[i][j][1])
+              }
+            }
+          }
+        }
+        return valuesArr;
+      }
+}
   
